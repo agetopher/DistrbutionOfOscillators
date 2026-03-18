7531dd2ada61289cdccb5e477809f81669ea0953
agetopher <agesenc@gmail.com>
2026-03-18 12:42:46 -0400

    added initial files and the mutual inhibition test

---
+++ functions.py
@@ -0,0 +1,45 @@
+import numpy as np
+import settings
+
+# Model Equations
+
+# Synaptic Fatigue
+def sf_vec(v, s):
+    conditions = [
+        (v > settings.L) & (s > 0),
+        (v <= settings.L) & (s < 1)
+    ]
+    choices = [
+        -settings.a*(v - settings.L),
+        settings.b
+    ]
+    return np.select(conditions, choices)
+
+def f_vec(v):
+    conditions = [
+        v > settings.H, 
+        v >= settings.T, 
+        v >= settings.L
+    ]
+    choices = [
+        settings.m4*(-v+settings.H), 
+        settings.m3*(v-settings.H)*(v-settings.T), 
+        settings.m2*(v-settings.L)*(v-settings.T)
+    ]
+    return np.select(conditions, choices, default=settings.m1*(-v+settings.L)).reshape(settings.numCells,)
+
+def f(v):
+    if (v > settings.H):
+        return settings.m4*(-v+settings.H)
+    elif (v >= settings.T):
+        return settings.m3*(v-settings.H)*(v-settings.T)
+    elif (v >= settings.L):
+        return settings.m2*(v-settings.L)*(v-settings.T)
+    else:
+        return settings.m1*(-v+settings.L)
+
+'''
+Loss functions to consider
+\distance^\alpha_0 * \kappa(s)^\alpha_1 * \var{\kapaa(s)_s)^{ -\apha_2} * \lambda^\alpha_3
+\distance^\alpha_0  +  \kappa(s)^\alpha_1 * \var{\kapaa(s)_s) ^{ -\apha_2} * \lambda ^\alpha_3
+'''
